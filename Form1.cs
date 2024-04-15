a447ffa88cdab8101108b5cc6a5fb89d0dd74e10
Marcos <marcoshr_baptista@hotmail.com>
2024-04-15 13:44:32 -0300

    thickness is working

--- Form1.cs
+++ Form1.cs
@@ -6,11 +6,12 @@ namespace HandWriteRecognize
 {
     public partial class Form1 : Form
     {
-        PictureBox pb;
+        PictureBox pb = new PictureBox();
         Bitmap bmp;
         Graphics g;
         private bool isDrawing = false;
         private Point previousPoint;
+        private int thickness = 5;
 
         public Form1()
         {
@@ -28,27 +29,37 @@ namespace HandWriteRecognize
             this.KeyDown += (o, e) =>
             {
                 if (e.KeyCode == Keys.Escape)
-                {
                     Application.Exit();
-                }
+
+                if (e.KeyCode == Keys.Up)
+                    this.thickness++;
+
+                if (e.KeyCode == Keys.Down)
+                    this.thickness--;
+
+
             };
 
             this.Load += (o, e) =>
             {
-                this.bmp = new Bitmap(pb.Width, pb.Height);    
+                this.bmp = new Bitmap(pb.Width, pb.Height);
                 g = Graphics.FromImage(bmp);
                 g.Clear(Color.White);
                 this.pb.Image = bmp;
 
                 Onstart();
-                
+
             };
         }
 
         void Onstart()
         {
-            Controls controls = new Controls();
-            var trackBar = controls.TrackBar1;
+            string thicknessText = "Hello, WinForms!";
+            Font font = new Font("Arial", 12);
+            Brush brush = Brushes.Black;
+            PointF point = new PointF(10, 10);
+            g.DrawString(thicknessText, font, brush, point);
+
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
@@ -57,13 +68,17 @@ namespace HandWriteRecognize
             previousPoint = e.Location;
         }
 
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
         private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
             if (isDrawing)
             {
                 using (Graphics g = this.CreateGraphics())
                 {
-                    using (Pen pen = new Pen(Color.Black, 5))
+                    using (Pen pen = new Pen(Color.Black, thickness))
                     {
                         g.DrawLine(pen, previousPoint, e.Location);
                     }
@@ -72,9 +87,6 @@ namespace HandWriteRecognize
             }
         }
 
-        private void panel1_MouseUp(object sender, MouseEventArgs e)
-        {
-            isDrawing = false;
-        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,30 +2,30 @@ using System;
 using System.IO;
 using HandWriteRecognize;
 
-string csv = "./english.csv";
+// string csv = "./english.csv";
 
-string[] lines = File.ReadAllLines(csv);
-string[] cleanLines = new string[lines.Length - 1];
-Array.Copy(lines, 1, cleanLines, 0, cleanLines.Length);
+// string[] lines = File.ReadAllLines(csv);
+// string[] cleanLines = new string[lines.Length - 1];
+// Array.Copy(lines, 1, cleanLines, 0, cleanLines.Length);
 
-foreach (string line in cleanLines)
-{
-    string[] parts = line.Split(',');
-    string imgName = parts[0];
-    string character = imgName.Substring(8, 2);
+// foreach (string line in cleanLines)
+// {
+//     string[] parts = line.Split(',');
+//     string imgName = parts[0];
+//     string character = imgName.Substring(8, 2);
 
-    character = $"Img/{character}/";
+//     character = $"Img/{character}/";
 
-    MoveImage(imgName, character);
-}
-MessageBox.Show("Done");
+//     MoveImage(imgName, character);
+// }
+// MessageBox.Show("Done");
 
-void MoveImage(string origin, string destiny)
-{
-    if(!Directory.Exists(destiny))
-        Directory.CreateDirectory(destiny);
-    File.Move(origin, destiny + Path.GetFileName(origin));
-}
+// void MoveImage(string origin, string destiny)
+// {
+//     if(!Directory.Exists(destiny))
+//         Directory.CreateDirectory(destiny);
+//     File.Move(origin, destiny + Path.GetFileName(origin));
+// }
 
 var form = new Form1();
 
