47553cc3ea96a25acf3f9cd37a24b5805a23edb6
Marcos <marcoshr_baptista@hotmail.com>
2024-04-15 16:56:05 -0300

    TA FUNCIONANDO

--- python.cs
+++ python.cs
@@ -1,35 +1,35 @@
-using System;
-using System.Windows.Forms;
-using Python.Runtime;
+// using System;
+// using System.Windows.Forms;
+// using Python.Runtime;
 
-Runtime.PythonDLL = "python311.dll";
-PythonEngine.Initialize();
+// Runtime.PythonDLL = "python311.dll";
+// PythonEngine.Initialize();
 
-dynamic tf = Py.Import("tensorflow");
-dynamic np = Py.Import("numpy");
+// dynamic tf = Py.Import("tensorflow");
+// dynamic np = Py.Import("numpy");
 
-dynamic model = tf.keras.models.load_model("checkpoints/crop-91-85.keras");
+// dynamic model = tf.keras.models.load_model("checkpoints/crop-91-85.keras");
 
-// pip install pillow
-dynamic list = new PyList();
-list.append(tf.keras.utils.load_img("imgTest/f3.png"));
-list.append(tf.keras.utils.load_img("imgTest/f3.png"));
-dynamic data = np.array(list);
+// // pip install pillow
+// dynamic list = new PyList();
+// list.append(tf.keras.utils.load_img("imgTest/f3.png"));
+// list.append(tf.keras.utils.load_img("imgTest/f3.png"));
+// dynamic data = np.array(list);
 
-MessageBox.Show("Chegou");
+// MessageBox.Show("Chegou");
 
-// dynamic result = model.predict(data);
-// MessageBox.Show((np.argmax(result) + 1).ToString());
+// // dynamic result = model.predict(data);
+// // MessageBox.Show((np.argmax(result) + 1).ToString());
 
-try
-{
-    dynamic result = model.predict(data);
-    MessageBox.Show((np.argmax(result) + 1).ToString());
-}
-catch (Exception ex)
-{
-    MessageBox.Show("An error occurred: " + ex.Message);
-}
+// try
+// {
+//     dynamic result = model.predict(data);
+//     MessageBox.Show((np.argmax(result) + 1).ToString());
+// }
+// catch (Exception ex)
+// {
+//     MessageBox.Show("An error occurred: " + ex.Message);
+// }
 
 
-PythonEngine.Shutdown();
+// PythonEngine.Shutdown();
